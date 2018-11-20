da2cc0305515515b031f5d28a4fc9b2986b5e57f
Alexey <oberonru@mail.ru>
2018-11-20 17:08:41 +0600

    Добавил джавадоки

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,8 +1,12 @@
 package ru.job4j.calculate;
 
 public class Calculate {
-	
+	 /**
+     * Main.
+     * @param args - args.
+     */
 	public static void main(String[] args){
 		System.out.println("Hello, World!");
 	}
+	
 }
