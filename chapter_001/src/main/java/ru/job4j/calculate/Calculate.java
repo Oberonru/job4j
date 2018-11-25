376c1f6f04cd39794774230154bbab15f4575f97
Alexey <oberonru@mail.ru>
2018-11-25 15:56:19 +0600

    3.1 Элементарный калькулятор

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,23 +1,52 @@
 package ru.job4j.calculate;
+
 /**
-*class Calculator.
-*@autor Alexey Petrushenko
-*since 21.11.2018
-*/
+ * class Calculator.
+ *
+ * @autor Alexey Petrushenko
+ * since 21.11.2018
+ */
 public class Calculate {
-	 /**
+    /**
      * Main.
+     *
      * @param args - args.
      */
-	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-	}
-	 /**
+
+    private double result = 0.0;
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    /**
      * Method echo.
+     *
      * @param name Your name.
      * @return Echo plus your name.
      */
     public String echo(String name) {
         return "Echo, echo, echo : " + name;
     }
+
+    public void add(double first, double second) {
+        this.result = first + second;
+    }
+
+	public void substract(double first, double second) {
+        this.result = first - second;
+    }
+
+    public void div (double first, double second) {
+        this.result = first/second;
+    }
+
+    public void multiple (double first, double second) {
+        this.result = first*second;
+    }
+
+    public double getResult() {
+        return  this.result;
+    }
+
 }
--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -1,28 +1,66 @@
 package ru.job4j.calculate;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+
 /**
-*Test
-*
-*@autor Alexey Petrushenko
-*version 1.0.0
-*@since 20.11.2018
-*/
+ * Test
+ *
+ * @autor Alexey Petrushenko
+ * version 1.0.0
+ * @since 20.11.2018
+ */
 
 public class CalculateTest {
 
-/**
-*Test eho
-*/	
+    /**
+     * Test eho
+     */
 
-@Test
-public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Alexey Petrushenko";
-    String expect = "Echo, echo, echo :  Alexey Petrushenko"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
+    @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Alexey Petrushenko";
+        String expect = "Echo, echo, echo : Alexey Petrushenko";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenAddOnePlusOneThenTwo() {
+        Calculate calc = new Calculate();
+        calc.add(1D, 1D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void testSubstract() {
+        Calculate calculate = new Calculate();
+        calculate.substract(5D, 1D);
+        double result = calculate.getResult();
+        double expected = 4D;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void testDiv() {
+        Calculate calculate = new Calculate();
+        calculate.div(4D, 2D);
+        double result = calculate.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void testMultiple() {
+        Calculate calculate = new Calculate();
+        calculate.multiple(5D, 5D);
+        double result = calculate.getResult();
+        double expected = 25D;
+        assertThat(result, is(expected));
+    }
 }
