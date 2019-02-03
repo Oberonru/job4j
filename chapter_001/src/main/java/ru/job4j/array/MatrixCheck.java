bfccd98bd8945d8c774ef5799d250194fb481e83
Alexey <oberonru@mail.ru>
2019-02-03 18:30:12 +0600

    Ошибки только в puzzle

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -5,11 +5,11 @@ public class MatrixCheck {
     public static boolean mono(boolean[][] data) {
         boolean result = true;
         for (int i = 0; i < data.length - 1; i++) {
-            if (data[data.length -1][data.length - 1] != data[data.length - 1 - i][data.length - 1 - i]) {
+            if (data[data.length - 1][data.length - 1] != data[data.length - 1 - i][data.length - 1 - i]) {
                 result = false;
                 break;
             }
-            if (data[i][i] != data[i + 1][i + 1])   {
+            if (data[i][i] != data[i + 1][i + 1]) {
                 result = false;
                 break;
             }
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.array;
+
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -8,28 +10,30 @@ public class FindLoopTest {
     @Test
     public void whenArrayHasLengh5Then0() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3};
+        int[] input = new int[]{5, 10, 3};
         int value = 5;
         int result = find.indexOf(input, value);
         int expect = 0;
         assertThat(result, is(expect));
     }
+
     @Test
     public void testIndexOf() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3};
+        int[] input = new int[]{5, 10, 3};
         int value = 3;
         int result = find.indexOf(input, value);
         int expect = 2;
         assertThat(result, is(expect));
     }
+
     @Test
     public void testIndexOf2() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 13};
+        int[] input = new int[]{5, 10, 13};
         int value = 1;
         int result = find.indexOf(input, value);
-        int expect = 1;
+        int expect = -1;
         assertThat(result, is(expect));
     }
 }
