91e4c90d9076e2440e61c8ac6a5c962002531b82
Alexey <oberonru@mail.ru>
2019-02-06 17:18:23 +0600

    Слияние массивов

---
+++ chapter_001/src/main/java/ru/job4j/array/ArraysMerger.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class ArraysMerger {
+    public void arraysMerger(int[] array1, int[] array2) {
+        int[] newArray = new int[array1.length + array2.length];
+        System.arraycopy(array1, 0, newArray, 0, array1.length);
+        System.arraycopy(array2, 0, newArray, array1.length, array2.length);
+        Arrays.sort(newArray);
+    }
+
+}
