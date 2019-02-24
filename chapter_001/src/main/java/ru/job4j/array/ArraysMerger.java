77f0bbf04d53718e6b42f1480954fad11981ce94
Alexey <oberonru@mail.ru>
2019-02-24 17:50:35 +0600

    1.Слияние отсортированных массивов в сортированный массив.[#100167]

--- chapter_001/src/main/java/ru/job4j/array/ArraysMerger.java
+++ chapter_001/src/main/java/ru/job4j/array/ArraysMerger.java
@@ -5,10 +5,24 @@ import java.util.Arrays;
 public class ArraysMerger {
     public int[] arraysMerger(int[] array1, int[] array2) {
         int[] newArray = new int[array1.length + array2.length];
-        System.arraycopy(array1, 0, newArray, 0, array1.length);
-        System.arraycopy(array2, 0, newArray, array1.length, array2.length);
-        Arrays.sort(newArray);
+        int i = 0, j = 0;
+        for (int k = 0; k < newArray.length; k++) {
+            if (i > array1.length - 1) {
+                newArray[k] = array2[j];
+                i++;
+            }
+            else if (j > array2.length - 1) {
+                newArray[k] = array1[i];
+            }
+            else if (array1[i] < array2[j]) {
+                newArray[k] = array1[i];
+                i++;
+            }
+            else {
+                newArray[k] = array2[j];
+                j++;
+            }
+        }
         return newArray;
     }
-
 }
